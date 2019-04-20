493442177da1078dfefae50fcb1b1f0c7133e555
5pence <sbarriball@me.com>
2019-04-20 16:48:12 +0100

    setup dispatcher servlet

---
+++ src/main/java/spencer/barriball/config/WebConfig.java
@@ -0,0 +1,11 @@
+package spencer.barriball.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "spencer.barriball")
+public class WebConfig {
+}
